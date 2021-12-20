1198611ea4f9738b263f788aa7ae24602eae8e1f
Taliya Shitreet <taliyashitreet@gmail.com>
2021-12-20 09:47:05 +0200

    balagan

--- src/DiGraph.py
+++ src/DiGraph.py
@@ -1,7 +1,5 @@
 from GraphInterface import GraphInterface
 
-"""renanaaaaaaaaaaaaaaaaaaaa CIIIIIIIIIIIIIII"""
-
 
 class DigGraph(GraphInterface):
     def _init_(self):
@@ -12,13 +10,22 @@ class DigGraph(GraphInterface):
         self.MC = 0
 
     def v_size(self) -> int:
-        pass
+        return len(self.nodes)
 
     def e_size(self) -> int:
-        pass
+        return self.edgesAmount
 
     def get_mc(self) -> int:
-        pass
+        return self.MC
+
+    def get_all_v(self) -> dict:
+        return self.nodes
+
+    def all_in_edges_of_node(self, id1: int) -> dict:
+        return self.parents[id1]
+
+    def all_out_edges_of_node(self, id1: int) -> dict:
+        return self.children[id1]
 
     def add_edge(self, id1: int, id2: int, weight: float) -> bool:
         if id1 and id2 in self.nodes and id1 != id2:
@@ -41,7 +48,28 @@ class DigGraph(GraphInterface):
             return False
 
     def remove_node(self, node_id: int) -> bool:
-        pass
+        if node_id in self.nodes:
+            childrenTmp = self.all_out_edges_of_node(node_id)
+            parentsTmp = self.all_in_edges_of_node(node_id)
+
+            for n in childrenTmp:
+                self.remove_edge(node_id, n)
+                del self.children[n][node_id]
+                self.MC+=1
+            for n in parentsTmp:
+                del self.parents[n][node_id]
+                self.MC+=1
+            del self.nodes[node_id]
+            return True
+        else: return False
 
     def remove_edge(self, node_id1: int, node_id2: int) -> bool:
-        pass
+        if node_id1 and node_id2 in self.nodes\
+         and node_id1 in self.children and node_id2 in self.parents:
+            del self.parents[node_id2][node_id1]
+            del self.children[node_id1][node_id2]
+            self.MC += 1
+            self.edgesAmount -= 1
+            return True
+        else:
+            return False
